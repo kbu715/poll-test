9483e118798da2e68bb99013211ec0bda0ff1ce3
kbu715 <kbu715@gmail.com>
2020-06-11 00:35:07 +0900

    0611 last test

--- index.js
+++ index.js
@@ -3,60 +3,60 @@ const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 app.set('view engine', 'ejs')
-mongoose.connect('mongodb+srv://tiger:1234@cluster0-cgecu.mongodb.net/test?retryWrites=true&w=majority',{useNewUrlParser: true},
-function(err){
-    if(err){
-        console.log(err)
-    } else{
-        console.log('connected')
-    }
-
-})
-
-const pollEx = new mongoose.Schema({
-    gender:String,
-    animal:String,
-})
-const poll = mongoose.model('poll',pollEx)
-
-
-
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(express.static('public'))
-app.get('/', (req,res)=>{
-    res.sendFile('index.html')
-})
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static(__dirname + '/public'));
+// DB setting
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect("mongodb+srv://tiger:1234@cluster0-cgecu.mongodb.net/test?retryWrites=true&w=majority");
+var db = mongoose.connection;
+db.once('open', function () {
+  console.log('DB connected');
+});
+db.on('error', function (err) {
+  console.log('DB ERROR : ', err);
+});
 
-app.get('/pollAction', (req,res)=>{
-    console.log(req.query)
-    const pollModel = new poll()
-    pollModel.gender = req.query.gender;
-    pollModel.animal = req.query.animal;
-    pollModel.save()
-    .then(()=>{
-        console.log("완료")
-        let arr = []
-        poll.countDocuments({gender:"남", animal:"호랑이"}, (err,c)=>{
-            arr.push(c);
-            poll.countDocuments({gender:"남", animal:"코끼리"}, (err,c)=>{
-                arr.push(c);
-                poll.countDocuments({gender:"여", animal:"호랑이"}, (err,c)=>{
-                    arr.push(c);
-                    poll.countDocuments({gender:"여", animal:"코끼리"}, (err,c)=>{
-                        arr.push(c);
-                        res.render('index',{data:arr})
-                    })
-                })        
-            })
-        })
-        
-    })
-    .catch(err =>{
-        if(err){
-            res.send(err)
-        }
-        })    
-})
 
-app.listen(3000)
+const pollSchema = mongoose.Schema({
+    gender: { type: String },
+    animal: { type: String },
+  }
+  );
+const Poll = mongoose.model('poll',pollSchema)
+
+
+
+app.get('/', function (req, res) {
+    res.render('new')
+  });
+  
+ 
+  app.get('/pollAction', async function (req, res) {
+    let result = [
+      await Poll.countDocuments({ gender: '남' ,animal:'호랑이'}),
+      await Poll.countDocuments({ gender: '남' ,animal:'코끼리'}),
+      await Poll.countDocuments({ gender: '여' ,animal:'호랑이'}),
+      await Poll.countDocuments({ gender: '여' ,animal:'코끼리'}),
+    ]
+    res.render('index',{data:result})
+    
+  }),
+
+  
+  // Contacts - create
+  app.post('/pollAction', function (req, res) {
+    Poll.create(req.body, function (err) {
+      if (err) return res.json(err);
+      res.redirect('/pollAction');
+    });
+  });
+  
+  // Port setting
+  var port = 3000;
+  app.listen(port, function () {
+    console.log('server on! http://localhost:' + port);
+  });
